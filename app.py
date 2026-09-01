e84286b0b283db568b3879693a769c7efc6d969e
avasht-web <avashthapa123456789@gmail.com>
2026-09-01 13:46:12 +0545

    tasks added

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+@app.route('/tasks')
+def tasks():
+    return render_template('tasks.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
