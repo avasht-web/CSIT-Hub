dc6db9345c743722fcce694314b1032712e94a17
avasht-web <avashthapa123456789@gmail.com>
2026-09-14 13:27:28 +0545

    sqlite

--- python/app.py
+++ python/app.py
@@ -1,8 +1,20 @@
 ﻿from flask import Flask, render_template, request
 import json 
+import sqlite3
 app=Flask(__name__)
 with open("tasks.json", "r") as file:
     taskl=json.load(file)
+def database():
+    conn=sqlite3.connect("../database/tasks.db")
+    conn.row_factory=sqlite3.Row
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS tasktable(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    completed INTEGER NOT NULL DEFAULT 0)
+    """)
+    conn.commit()
+    return conn
 @app.route('/')
 @app.route('/home')
 def home():
@@ -15,9 +27,15 @@ def contact():
     return render_template('contact.html')
 @app.route('/tasks',methods=["GET", "POST"])
 def tasks():
-    if request.method=="POST": 
+    if request.method=="POST":
         task=request.form["task"]
-        taskl.append(task)    
+        conn=database()
+        conn.execute("INSERT INTO tasktable(title,completed) VALUES (?,?)", (task, 0))
+        conn.commit() 
+        conn.close()
+       
+        taskl.append(task)
+           
         with open("tasks.json", "w") as file:
             json.dump(taskl, file)
     return render_template('tasks.html', tasks=taskl)
