480d94a5107a2ba652ce65671f35ec4a0f4c67bb
avasht-web <avashthapa123456789@gmail.com>
2026-09-18 17:46:18 +0545

    SQLITE used as database and removed json as the database

--- main/app.py
+++ main/app.py
@@ -1,9 +1,8 @@
 ﻿from flask import Flask, render_template, request
 import json 
 import sqlite3
-app=Flask(__name__)
-with open("tasks.json", "r") as file:
-    taskl=json.load(file)
+app=Flask(__name__, template_folder="../templates", static_folder="../static")
+
 def database():
     conn=sqlite3.connect("../database/tasks.db")
     conn.row_factory=sqlite3.Row
@@ -15,30 +14,31 @@ def database():
     """)
     conn.commit()
     return conn
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 @app.route('/tasks',methods=["GET", "POST"])
 def tasks():
+    conn=database()
     if request.method=="POST":
         task=request.form["task"]
-        conn=database()
         conn.execute("INSERT INTO tasktable(title,completed) VALUES (?,?)", (task, 0))
-        conn.commit() 
-        conn.close()
-       
-        taskl.append(task)
-           
-        with open("tasks.json", "w") as file:
-            json.dump(taskl, file)
-    return render_template('tasks.html', tasks=taskl)
+        conn.commit()
+    tasks=conn.execute("SELECT *FROM tasktable").fetchall()
+    conn.close()
+    return render_template('tasks.html', tasks=tasks)
+     
 @app.route('/api/student')
 def student():
     return {
@@ -46,5 +46,6 @@ def student():
     "college": "Madan Bhandari",
     "course": "BSc CSIT"
     }
+
 if __name__ == '__main__':
     app.run(debug=True)
